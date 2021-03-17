1a6202a1e2c31cf2d65340ac8fd41014fa23ff71
Elisheva Wechsler <ewechsle@g.jct.ac.il>
2021-03-17 22:46:10 +0200

    finish all the classes

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,5 +1,18 @@
 package geometries;
 
-public class Triangle {
+import primitives.Point3D;
+import primitives.Vector;
+
+public class Triangle extends Polygon {
+
+    public Triangle(Point3D a, Point3D b , Point3D c )
+    {
+        super(a,b,c);
+    }
+
+    public Vector getNormal(Point3D p)
+    {
+        return null;
+    }
 
 }

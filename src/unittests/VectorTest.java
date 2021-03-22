5e1125647734e25936090f02fa9e190e6845e5a4
noadahlia <ghozlan@g.jct.ac.il>
2021-03-22 21:13:04 +0100

    FirstCommit Noa

---
+++ src/unittests/VectorTest.java
@@ -0,0 +1,50 @@
+package unittests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void substract() {
+    }
+
+    @Test
+    public void testSubstract() {
+    }
+
+    @Test
+    public void scale() {
+    }
+
+    @Test
+    public void dotProduct() {
+    }
+
+    @Test
+    public void crossProduct() {
+    }
+
+    @Test
+    public void lengthSquared() {
+    }
+
+    @Test
+    public void length() {
+    }
+
+    @Test
+    public void normalized() {
+    }
+
+    @Test
+    public void normalize() {
+    }
+
+
+}

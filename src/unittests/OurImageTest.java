770bb9170305ddb206459a678c46d7c0b8cc3293
noadahlia <ghozlan@g.jct.ac.il>
2021-06-01 12:53:41 +0300

    Change image

--- src/unittests/OurImageTest.java
+++ src/unittests/OurImageTest.java
@@ -16,25 +16,30 @@ public class OurImageTest {
 
 
     @Test
-    public void createImage()
-    {
+    public void createImage() {
         Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setViewPlaneSize(200, 200).setDistance(1000);
-        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.PINK), 0.2));
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2))
+                .setBackground(new Color(java.awt.Color.PINK));
 
         scene.geometries.add(//
-                new Sphere(new Point3D(50,50,-45),40)//
-                .setEmission(new Color (java.awt.Color.MAGENTA))//
+                new Sphere(new Point3D(50, 50, -45), 40)//
+                        .setEmission(new Color(java.awt.Color.MAGENTA))//
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
-                new Sphere(new Point3D(10,10,-5),30)//
-                        .setEmission(new Color (java.awt.Color.BLUE))//
+                new Sphere(new Point3D(10, 10, -5), 30)//
+                        .setEmission(new Color(java.awt.Color.BLUE))//
                         .setMaterial(new Material().setKd(0.1).setKs(0.1).setShininess(80).setKt(0.4)),
-              new Sphere(new Point3D(-50,-50,-30),20)//
-                .setEmission(new Color (java.awt.Color.YELLOW))//
-                .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(10).setKt(0.2)),
-        new Triangle(new Point3D(-100, -100, -100), new Point3D(-70, 70, -140), new Point3D(75, 75, -150)) //
-                .setEmission(new Color(java.awt.Color.RED)) //
-                .setMaterial(new Material().setKd(0.1).setKs(0.5).setShininess(100)));
+                new Sphere(new Point3D(-50, -50, -30), 20)//
+                        .setEmission(new Color(java.awt.Color.YELLOW))//
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(10).setKt(0.2)),
+                new Triangle(new Point3D(-100, -100, -100), new Point3D(-70, 70, -140), new Point3D(75, 75, -150)) //
+                        .setEmission(new Color(java.awt.Color.RED)) //
+                        .setMaterial(new Material().setKd(0.1).setKs(0.5).setShininess(100)),
+                new Sphere(new Point3D(80, 40, -4), 10)//
+                        .setEmission(new Color(java.awt.Color.CYAN))//
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)));
+
+        // ;
 
 
         scene.lights.add( //
@@ -50,8 +55,6 @@ public class OurImageTest {
 
         render.renderImage();
         render.writeToImage();
-
-                // )
 
     }
 

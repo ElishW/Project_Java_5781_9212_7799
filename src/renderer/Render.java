8b267bbdc2f4a98cc9b41fc50c0bbcc60e5cf903
Elisheva Wechsler <ewechsle@g.jct.ac.il>
2021-06-19 23:49:56 +0300

    Final project2

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -1,6 +1,6 @@
 package renderer;
 
-import elements.Camera;
+import elements.*;
 import scene.Scene;
 import primitives.*;
 
@@ -10,6 +10,149 @@ public class Render {
     ImageWriter imageWriter;
     Camera camera;
     RayTracerBase rayTracer;
+
+    private static final String RESOURCE_ERROR = "Renderer resource not set";
+    private static final String RENDER_CLASS = "Render";
+    private static final String IMAGE_WRITER_COMPONENT = "Image writer";
+    private static final String CAMERA_COMPONENT = "Camera";
+    private static final String RAY_TRACER_COMPONENT = "Ray tracer";
+
+    private int threadsCount = 0;
+    private static final int SPARE_THREADS = 2; // Spare threads if trying to use all the cores
+    private boolean print = false; // printing progress percentage
+
+    public Render setMultithreading(int threads) {
+        if (threads < 0)
+            throw new IllegalArgumentException("Multithreading parameter must be 0 or higher");
+        if (threads != 0)
+            this.threadsCount = threads;
+        else {
+            int cores = Runtime.getRuntime().availableProcessors() - SPARE_THREADS;
+            this.threadsCount = cores <= 2 ? 1 : cores;
+        }
+        return this;
+    }
+    public Render setDebugPrint() {
+        print = true;
+        return this;
+    }
+
+
+    private class Pixel{
+        private long maxRows = 0;
+        private long maxCols = 0;
+        private long pixels = 0;
+        public volatile int row = 0;
+        public volatile int col = -1;
+        private long counter = 0;
+        private int percents = 0;
+        private long nextCounter = 0;
+
+        /**
+         * The constructor for initializing the main follow up Pixel object
+         *
+         * @param maxRows the amount of pixel rows
+         * @param maxCols the amount of pixel columns
+         */
+        public Pixel(int maxRows, int maxCols) {
+        this.maxRows = maxRows;
+        this.maxCols = maxCols;
+        this.pixels = (long) maxRows * maxCols;
+        this.nextCounter = this.pixels / 100;
+        if (Render.this.print)
+            System.out.printf("\r %02d%%", this.percents);
+    }
+
+        /**
+         * Default constructor for secondary Pixel objects
+         */
+        public Pixel() {
+    }
+
+        /**
+         * Internal function for thread-safe manipulating of main follow up Pixel object
+         * - this function is critical section for all the threads, and main Pixel
+         * object data is the shared data of this critical section.<br/>
+         * The function provides next pixel number each call.
+         *
+         * @param target target secondary Pixel object to copy the row/column of the
+         *               next pixel
+         * @return the progress percentage for follow up: if it is 0 - nothing to print,
+         *         if it is -1 - the task is finished, any other value - the progress
+         *         percentage (only when it changes)
+         */
+        private synchronized int nextP(Pixel target) {
+        ++col;
+        ++this.counter;
+        if (col < this.maxCols) {
+            target.row = this.row;
+            target.col = this.col;
+            if (Render.this.print && this.counter == this.nextCounter) {
+                ++this.percents;
+                this.nextCounter = this.pixels * (this.percents + 1) / 100;
+                return this.percents;
+            }
+            return 0;
+        }
+        ++row;
+        if (row < this.maxRows) {
+            col = 0;
+            target.row = this.row;
+            target.col = this.col;
+            if (Render.this.print && this.counter == this.nextCounter) {
+                ++this.percents;
+                this.nextCounter = this.pixels * (this.percents + 1) / 100;
+                return this.percents;
+            }
+            return 0;
+        }
+        return -1;
+    }
+
+        /**
+         * Public function for getting next pixel number into secondary Pixel object.
+         * The function prints also progress percentage in the console window.
+         *
+         * @param target target secondary Pixel object to copy the row/column of the
+         *               next pixel
+         * @return true if the work still in progress, -1 if it's done
+         */
+        public boolean nextPixel(Pixel target) {
+        int percent = nextP(target);
+        if (Render.this.print && percent > 0)
+            synchronized (this) {
+                notifyAll();
+            }
+        if (percent >= 0)
+            return true;
+        if (Render.this.print)
+            synchronized (this) {
+                notifyAll();
+            }
+        return false;
+    }
+
+        /**
+         * Debug print of progress percentage - must be run from the main thread
+         */
+        public void print() {
+        if (Render.this.print)
+            while (this.percents < 100)
+                try {
+                    synchronized (this) {
+                        wait();
+                    }
+                    System.out.printf("\r %02d%%", this.percents);
+                    System.out.flush();
+                } catch (Exception e) {
+                }
+    }
+    }
+    /**
+     * Image, RayTracer,Camera... implementations
+     * @param
+     * @return
+     */
 
     public Render setImageWriter(ImageWriter _imageWriter) {
         imageWriter = _imageWriter;
@@ -43,56 +186,112 @@ public class Render {
             }
 
             //rendering the image
-            int nX = imageWriter.getNx()*2;
-            int nY = imageWriter.getNy()*2;
-            for (int i = 0; i < nY; i+=2) {
-                for (int j = 0; j < nX; j+=2) {
-                    Ray rays[] = {camera.constructRayThroughPixel(nX, nY, j + 1, i),
-                            camera.constructRayThroughPixel(nX, nY, j, i + 1),
-                            camera.constructRayThroughPixel(nX, nY, j + 1, i + 1),
-                            camera.constructRayThroughPixel(nX, nY, j, i)};
+            int nX = imageWriter.getNx() * 2;
+            int nY = imageWriter.getNy() * 2;
+            if (threadsCount == 0) {
+                for (int i = 0; i < nY; i += 2) {
+                    for (int j = 0; j < nX; j += 2) {
+                        Ray rays[] = {camera.constructRayThroughPixel(nX, nY, j + 1, i),
+                                camera.constructRayThroughPixel(nX, nY, j, i + 1),
+                                camera.constructRayThroughPixel(nX, nY, j + 1, i + 1),
+                                camera.constructRayThroughPixel(nX, nY, j, i)};
 
-                    Color pixelColor = Color.BLACK;
+                        Color pixelColor = Color.BLACK;
 
-                    for (Ray r : rays) {
-                        pixelColor = pixelColor.add(rayTracer.traceRay(r));
+                        for (Ray r : rays) {
+                            pixelColor = pixelColor.add(rayTracer.traceRay(r));
+                        }
 
+
+                        imageWriter.writePixel(j / 2, i / 2, pixelColor.reduce(4));
                     }
 
-                    imageWriter.writePixel(j/2, i/2, pixelColor.reduce(4));
                 }
-        }
-        } catch (MissingResourceException e) {
+            } else
+                renderImageThreaded();
+        }  catch (MissingResourceException e) {
             throw new UnsupportedOperationException("Not implemented yet" + e.getClassName());
         }
     }
 
 
-    public void printGrid ( int interval, Color color)
-    {
+    public void printGrid(int interval, Color color) {
         if (imageWriter == null)
             throw new MissingResourceException("missing imageWriter", ImageWriter.class.getName(), "");
         int nX = imageWriter.getNx();
         int nY = imageWriter.getNy();
 
-        for (int i = 0; i < nX; i +=interval) {
+        for (int i = 0; i < nX; i += interval) {
             for (int j = 0; j < nY; j++) {
                 imageWriter.writePixel(i, j, color);
             }
         }
-        for (int i = 0; i < nY; i +=interval) {
+        for (int i = 0; i < nY; i += interval) {
             for (int j = 0; j < nX; j++) {
                 imageWriter.writePixel(j, i, color);
             }
         }
     }
 
-    public void writeToImage ()
-    {
+    public void writeToImage() {
         if (imageWriter == null)
             throw new MissingResourceException("missing imageWriter", ImageWriter.class.getName(), "");
         imageWriter.writeToImage();
     }
 
 
+        private void castRay(int nX, int nY, int col, int row) {
+col*=2;
+row*=2;
+            nX*=2;
+            nY*=2;
+                    Ray rays[] = {camera.constructRayThroughPixel(nX, nY, col + 1, row),
+                            camera.constructRayThroughPixel(nX, nY, col, row + 1),
+                            camera.constructRayThroughPixel(nX, nY, col + 1, row + 1),
+                            camera.constructRayThroughPixel(nX, nY, col, row)};
+
+                    Color pixelColor = Color.BLACK;
+
+                    for (Ray r : rays) {
+                        pixelColor = pixelColor.add(rayTracer.traceRay(r));
+                    }
+            imageWriter.writePixel((long)(col+1) / 2, row / 2, pixelColor.reduce(4));
+            imageWriter.writePixel(col / 2, (row+1) / 2, pixelColor.reduce(4));
+            imageWriter.writePixel((col+1) / 2, (row+1) / 2, pixelColor.reduce(4));
+            imageWriter.writePixel(col / 2, row / 2, pixelColor.reduce(4));
+
+
+            }
+
+
+            private void renderImageThreaded () {
+                final int nX = imageWriter.getNx();
+                final int nY = imageWriter.getNy();
+                final Pixel thePixel = new Pixel(nY, nX);
+                // Generate threads
+                Thread[] threads = new Thread[threadsCount];
+                for (int x = threadsCount - 1; x >= 0; --x) {
+                    threads[x] = new Thread(() -> {
+                        Pixel pixel = new Pixel();
+                        while (thePixel.nextPixel(pixel))
+                            castRay(nX, nY, pixel.col, pixel.row);
+                    });
+                }
+                // Start threads
+                for (Thread thread : threads)
+                    thread.start();
+
+                // Print percents on the console
+                thePixel.print();
+
+                // Ensure all threads have finished
+                for (Thread thread : threads)
+                    try {
+                        thread.join();
+                    } catch (Exception e) {
+                    }
+
+                if (print)
+                    System.out.print("\r100%");
+            }
 }
--- src/unittests/MiniProjectRender1.java
+++ src/unittests/MiniProjectRender1.java
@@ -33,11 +33,12 @@ public class MiniProjectRender1 {
                 new Sphere(new Point3D(0, 30, 0), 80) //
                 .setEmission(new Color(0,0,0))//
                 .setMaterial(new Material().setKd(0.255).setKs(0.4).setShininess(50).setKr(0).setKt(1)),
-             /* new Triangle(new Point3D(-660, 640, 640), new Point3D(640, -660, 640),
-                     new Point3D(-577, -577, -810)) //
-                    .setEmission(new Color(20,20,20)) //
-                      .setMaterial(new Material().setKt(0.99)),*/
-        new Triangle(new Point3D(-40, -50, -40), new Point3D(40, -50, -40),
+
+                new Triangle(new Point3D(-660, 640, 640), new Point3D(640, -660, 640),
+                        new Point3D(-577, -577, -810)) //
+                        .setEmission(new Color(java.awt.Color.BLACK))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)), //
+                new Triangle(new Point3D(-40, -50, -40), new Point3D(40, -50, -40),
                 new Point3D(40, -80, -40))//
                 .setEmission(new Color(24,24,24)) //
                 .setMaterial(new Material().setKd(0.25).setKs(0.4).setShininess(50).setKr(0.1).setKt(1)),
@@ -77,9 +78,9 @@ public class MiniProjectRender1 {
                         new Point3D(40, -50, 40))//
                         .setEmission(new Color(24,24,24))
                         .setMaterial(new Material().setKd(0.25).setKs(0.4).setShininess(50).setKr(0.1).setKt(1)),
-        new Sphere(new Point3D(1,30,0),15)//
-        .setEmission(new Color(java.awt.Color.YELLOW))//
-                .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
+                new Sphere(new Point3D(1,30,0),15)//
+                        .setEmission(new Color(java.awt.Color.YELLOW))//
+                        .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
                 new Sphere(new Point3D(25,10,-30),4)//
                         .setEmission(new Color(219,95,194))//
                         .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
@@ -87,11 +88,19 @@ public class MiniProjectRender1 {
                         .setEmission(new Color(java.awt.Color.BLUE))//
                         .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
                 new Sphere(new Point3D(-40,20,-10),9)//
-                        .setEmission(new Color(java.awt.Color.RED))//
+                        .setEmission(new Color(21,132,15))//
                         .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
                 new Sphere(new Point3D(-60,35,-30),6)//
-                        .setEmission(new Color(45,221,222))//
-                        .setMaterial(new Material().setKr(0.4).setKt(0.4 )));
+                        .setEmission(new Color(13,166,195))//
+                        .setMaterial(new Material().setKr(0.4).setKt(0.4 )),
+                new Sphere(new Point3D(67,60,30),13)//
+                        .setEmission(new Color(195,60,13))//
+                        .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)),
+                new Sphere(new Point3D(-15,75,60),7)//
+                        .setEmission(new Color(198,234,49))//
+                        .setMaterial(new Material().setShininess(50).setKr(0.4).setKt(0.4)));
+
+        // making the stars
         for (var i=0; i < 200; i ++) {
             Random r = new Random();
             // min and max value of the radius
@@ -112,15 +121,16 @@ public class MiniProjectRender1 {
         }
 
        scene.lights.add( //
-                new SpotLight(new Color(java.awt.Color.WHITE),  new Point3D(-80, -90, 30), new Vector(2, 2, -6))//
+                new SpotLight(new Color(java.awt.Color.WHITE),  new Point3D(-80, -90, 1000), new Vector(2, 2, -6))//
                         .setKl(1E-5).setKq(1.5E-7));
         scene.lights.add( //
-                new SpotLight(new Color(java.awt.Color.WHITE),  new Point3D(30, 30, 10), new Vector(2, 2, -6))//
+                new SpotLight(new Color(java.awt.Color.WHITE),  new Point3D(30, 30, 1000), new Vector(2, 2, -6))//
                         .setKl(1E-5).setKq(1.5E-7));
 
 
 
-      scene.lights.add( //
+
+    /*scene.lights.add( //
                new PointLight(new Color(java.awt.Color.YELLOW),  new Point3D(1, 30, 0))//
                  .setKl(1E-5).setKq(1.5E-7));
         scene.lights.add( //
@@ -135,21 +145,29 @@ public class MiniProjectRender1 {
         scene.lights.add( //
                 new PointLight(new Color(45,221,222),  new Point3D(-60,35,-30))//
                         .setKl(1E-5).setKq(1.5E-7));
+        scene.lights.add( //
+                new PointLight(new Color(204,102,0),  new Point3D(67,60,30))//
+                        .setKl(1E-5).setKq(1.5E-7));
+        scene.lights.add( //
+                new PointLight(new Color(102,178,255),  new Point3D(-15,75,60))//
+                        .setKl(1E-5).setKq(1.5E-7));*/
         scene.lights.add(new PointLight(new Color(java.awt.Color.BLUE),new Point3D(0,-50,0)));
 
 
         Render render = new Render() //
-                .setImageWriter(new ImageWriter("Snow Ball", 500, 500)) //
+                .setImageWriter(new ImageWriter("Snow Ball", 1000, 1000)) //
                 .setCamera(camera) //
-                .setRayTracer(new RayTracerBasic(scene));
+                .setRayTracer(new RayTracerBasic(scene))//
+        .setMultithreading(0).setDebugPrint();
         render.renderImage();
         render.writeToImage();
 
-       camera.moveCamera(0,0,150);
+        camera.moveCamera(0,0,150);
         Render renderZ = new Render()//
                 .setImageWriter(new ImageWriter("Snow Ball Zoom", 500, 500)) //
                 .setCamera(camera) //
-                .setRayTracer(new RayTracerBasic(scene));
+                .setRayTracer(new RayTracerBasic(scene))//
+        .setMultithreading(3).setDebugPrint();
         renderZ.renderImage();
         renderZ.writeToImage();
 
